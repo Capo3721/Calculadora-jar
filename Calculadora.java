bfdec248f5f3163fc06460a7e3624f280fa66297
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2021-09-14 16:32:14 -0300

    Add files via upload
    Classe main da calculadora e a classe teste Unit.

---
+++ Calculadora.java
@@ -0,0 +1,16 @@
+package com.teste;
+
+public class Calculadora {
+	
+	public int subtrair(String expressao) {
+		int subtrai = 10;
+		for (String valorSubtrair : expressao.split("\\-"))
+			subtrai -= Integer.valueOf(valorSubtrair);
+			//System.out.println(subtrai);
+		
+		System.out.println(subtrai);
+		return subtrai;
+		
+	}
+
+}
---
+++ CalculadoraTest.java
@@ -0,0 +1,16 @@
+package com.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void testSubtrair() {
+		Calculadora calc = new Calculadora();
+		int subtrai= calc.subtrair("5-5");
+		assertEquals(0,subtrai);
+	}
+
+}
