d6ba1877b5b35423694fc19c638401160c2fdadc
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2021-09-20 10:39:19 -0300

    Calculadora - versão final JUNIT
    Script de teste do JUNIT 4.
    Versão final do projeto CALCULADORA, com 4 operadores: soma, divisão, multiplicação e subtração.

--- CalculadoraTest.java
+++ CalculadoraTest.java
@@ -1,4 +1,3 @@
-//teste Unit4
 package com.teste;
 
 import static org.junit.Assert.*;
@@ -10,8 +9,27 @@ public class CalculadoraTest {
 	@Test
 	public void testSubtrair() {
 		Calculadora calc = new Calculadora();
-		int subtrai= calc.subtrair("5-5");
-		assertEquals(0,subtrai);
+		float subtrai= calc.subtrair("5,5");
+		assertEquals(0,subtrai,0);
 	}
+	@Test
+	public void testSomar() {
+		Calculadora calc1 = new Calculadora();
+		int soma= calc1.somar("0,5");
+		assertEquals(15,soma);
+	}
+	@Test
+	public void testMultiplicar() {
+		Calculadora calc2 = new Calculadora();
+		int multi= calc2.multiplicar("3,5");
+		assertEquals(15,multi);
+	}
+	@Test
+	public void testDividir() {
+		Calculadora calc3 = new Calculadora();
+		int div= calc3.dividir("5");
+		assertEquals(2,div);
+	}
+	
 
 }
