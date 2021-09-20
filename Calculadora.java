2ff4337024df23879a096d6ece0b7048a50eec9d
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2021-09-20 10:37:55 -0300

    Calculadora - Versão final
    Teste JUNIT com as classes divisão, soma, multiplicação e subtração.
    Coloquei a classe subtração em FLOAT para mostrar a execução com floats no teste JUNIT. 
    As demais classes deixei como inteiro.

--- Calculadora.java
+++ Calculadora.java
@@ -1,17 +1,41 @@
-//Main
 package com.teste;
 
 public class Calculadora {
 	
-	public int subtrair(String expressao) {
-		int subtrai = 10;
-		for (String valorSubtrair : expressao.split("\\-"))
+	public float subtrair(String expressao) {
+		float subtrai = 10;
+		for (String valorSubtrair : expressao.split("\\,"))
 			subtrai -= Integer.valueOf(valorSubtrair);
-			//System.out.println(subtrai);
+			
 		
 		System.out.println(subtrai);
 		return subtrai;
 		
 	}
+	public int somar(String expressao) {
+	    int soma = 10;
+	    for (String valorSomar : expressao.split("\\,"))
+	    	soma += Integer.valueOf(valorSomar);
+			
+	    System.out.println(soma);
+	    return soma;
+	}
+	public int multiplicar(String expressao) {
+	    int multi = 1;
+	    for (String valorMultiplicar : expressao.split("\\,"))
+	    	multi *= Integer.valueOf(valorMultiplicar);
+			
+	    System.out.println(multi);
+	    return multi;
+	}
+	public int dividir(String expressao) {
+	    int div = 10;
+	    for (String valorDividir : expressao.split("\\,"))
+	    	div /= Integer.valueOf(valorDividir);
+			
+	    System.out.println(div);
+	    return div;
+	}
+		
 
 }
