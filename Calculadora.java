fd32ec7a71e5e955ebbc586443095bb85252154a
Arthur Prado <83094040+Capo3721@users.noreply.github.com>
2021-09-27 17:05:25 -0300

    Add files via upload
    Script calculadora em sua segunda versão.
    Alguns aprimoramentos foram feitos no script main:
    1- Substituição da tag vírgula por ponto e vírgula, afim de não criar conflitos em recebimento de valores float digitados por usuário. 
    3- Transformação de int para double em todas as funções da calculadora. 
    3- Remoção do print do código main.

--- Calculadora.java
+++ Calculadora.java
@@ -2,38 +2,38 @@ package com.teste;
 
 public class Calculadora {
 	
-	public float subtrair(String expressao) {
-		float subtrai = 10;
-		for (String valorSubtrair : expressao.split("\\,"))
-			subtrai -= Integer.valueOf(valorSubtrair);
+	public double subtrair(String expressao) {
+		double subtrai = -30;
+		for (String valorSubtrair : expressao.split("\\;"))
 			
+			subtrai -= Double.valueOf(valorSubtrair);
+			
+		System.out.println(expressao);
 		
-		System.out.println(subtrai);
 		return subtrai;
 		
 	}
-	public int somar(String expressao) {
-	    int soma = 10;
-	    for (String valorSomar : expressao.split("\\,"))
-	    	soma += Integer.valueOf(valorSomar);
+	public double somar(String expressao) {
+	    double soma = 40;
+	    for (String valorSomar : expressao.split("\\;"))
+	    	soma += Double.valueOf(valorSomar);
 			
-	    System.out.println(soma);
 	    return soma;
 	}
-	public int multiplicar(String expressao) {
-	    int multi = 1;
-	    for (String valorMultiplicar : expressao.split("\\,"))
-	    	multi *= Integer.valueOf(valorMultiplicar);
+	public double multiplicar(String expressao) {
+	    double multi = -1;
+	    multi *= 1;
+	    
+	    for (String valorMultiplicar : expressao.split("\\;"))
+	    	multi *= Double.valueOf(valorMultiplicar);
 			
-	    System.out.println(multi);
 	    return multi;
 	}
-	public int dividir(String expressao) {
-	    int div = 10;
-	    for (String valorDividir : expressao.split("\\,"))
+	public double dividir(String expressao) {
+	    double div = 10;
+	    for (String valorDividir : expressao.split("\\;"))
 	    	div /= Integer.valueOf(valorDividir);
 			
-	    System.out.println(div);
 	    return div;
 	}
 		
